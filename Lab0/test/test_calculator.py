9aa709b0813060fdd1ff1ff6a3c4bea0156617b8
Justin <justin.buss@dcmail.ca>
2025-01-16 20:40:31 -0500

    Changed numbers in test-calculator

--- Lab0/test/test_calculator.py
+++ Lab0/test/test_calculator.py
@@ -5,11 +5,11 @@ from Lab0.calculator import add, subtract
 
 class TestCalculator(unittest.TestCase):
     def test_add(self):
-        self.assertEqual(add(3, 4), 7)
+        self.assertEqual(add(7, 4), 7)
         self.assertEqual(add(-1, 1), 0)
 
     def test_subtract(self):
-        self.assertEqual(subtract(10, 5), 5)
+        self.assertEqual(subtract(20, 5), 5)
         self.assertEqual(subtract(0, 1), -1)
 
 
