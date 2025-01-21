80abc0d5488773530cbfe84b7ccaedcf50a71c84
AlexanderPapachristu <apapachristu@gmail.com>
2025-01-21 12:39:33 -0500

    added functionality to get_user_input function

--- Lab1.py
+++ Lab1.py
@@ -10,9 +10,8 @@ import string
 from random import shuffle
 from string import punctuation
 
-def get_user_input(prompt, min_value, max_value):
+MIN_LENGTH = 8
 
-    pass
 
 def generate_password(num_letters, num_digits, num_specials):
     #letter loop
@@ -38,3 +37,34 @@ def generate_password(num_letters, num_digits, num_specials):
             break
     random_password = letter_list + number_list + special_list
     return random_password
+
+def get_user_input():
+    sum = 0
+    num_letters = 0
+    num_digits = 0
+    num_specials = 0
+    # Getting input from user
+    try:
+        length = input("Enter the total length of the password: ")
+        length = int(length)
+        if length < MIN_LENGTH:
+            print(f"Please enter a value greater than {MIN_LENGTH}")
+        else:
+            get_user_input()
+        num_letters = input("Enter the number of letters desired in the password: ")
+        num_letters = int(num_letters)
+        sum += num_letters
+        num_digits = input("Enter the number of digits desired in the password: ")
+        num_digits = int(num_digits)
+        sum += num_digits
+        num_specials = input("Enter the number of special characters desired in the password: ")
+        num_specials = int(num_specials)
+        sum += num_specials
+    except:
+        print("Not a valid input. Please input an integer value.")
+        get_user_input()
+
+    generate_password(num_letters, num_digits, num_specials)
+    pass
+
+get_user_input()
